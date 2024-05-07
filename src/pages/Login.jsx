eac59e3118285509ee45decd8cccd72fda4549a3
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-05-07 17:45:45 +0530

    Add role base login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,88 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const theme = createTheme();
 
 export default function Login() {
   const navigate = useNavigate();
 
+  const [userRole, setUserRole] = useState();
+
   const [values, setValues] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   const { email, password } = values;
-
-  //   try {
-  //     const response = await axios.post('http://localhost:3001/api/auth/login', { email, password });
-  //     const token = response.data.token;
-  //     navigate('/home');
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
   useEffect(() => {
-    axios.get('http://localhost:3001/api/auth/authenticated')
-      .then(res => {
+    axios
+      .get("http://localhost:3001/api/auth/authenticated")
+      .then((res) => {
         if (res.data.authenticated) {
-          navigate('/details');
+          
+          setUserRole(res.data.user.role);
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
-  }, []);
-  
+  }, [navigate]);
 
   axios.defaults.withCredentials = true;
 
-  // useEffect(() => {
-  //   axios.get('http://localhost:3001')
-  //   .then (res => {
-  //     if(res.data.valid) {
-  //       navigate('/')
-  //     } else {
-  //       navigate('/login')
-  //     }
-  // })
-  // .catch(err => {
-  //   console.log(err)
-  // })
-  // }, [])
   const handleSubmit = async (event) => {
     event.preventDefault();
-    axios.post('http://localhost:3001/api/auth/login', values)
-    .then((response) => {
-      if (response.data.Login) {
-        navigate('/details');
-      } else {
-        alert("Email or Password is incorrect");
-      }
-      console.log(response);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    axios
+      .post("http://localhost:3001/api/auth/login", values)
+      .then((response) => {
+        if (response.data.Login) {
+          const userRole = response.data.user.role;
+          roleAuth(userRole); // Call the roleAuth function to navigate
+        } else {
+          alert("Email or Password is incorrect");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
-
+  
+  const roleAuth = (userRole) => {
+    switch (userRole) {
+      case "admin":
+        navigate("/admin/home");
+        break;
+      case "customer":
+        navigate("/customer/home");
+        break;
+      case "supplier":
+        navigate("/supplier/home");
+        break;
+      case "bailer":
+        navigate("/bailer/home");
+        break;
+      case "driver":
+        navigate("/driver/home");
+        break;
+      default:
+        break;
+    }
+  };
+  
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
@@ -94,18 +93,23 @@ export default function Login() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -115,7 +119,7 @@ export default function Login() {
               name="email"
               autoComplete="email"
               autoFocus
-              onChange={handleChange('email')}
+              onChange={handleChange("email")}
             />
             <TextField
               margin="normal"
@@ -126,13 +130,18 @@ export default function Login() {
               type="password"
               id="password"
               autoComplete="current-password"
-              onChange={handleChange('password')}
+              onChange={handleChange("password")}
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             />
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
               Sign In
             </Button>
             <Grid container>
