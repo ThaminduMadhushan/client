a6ac48434ea8e123da27830c8bcfe8c7fe86833d
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-05-12 14:00:28 +0530

    admin bin main page

---
+++ src/admin/AdminBin.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Sidenav from "../admin/adminComponents/AdminSidenav";
+import Navbar from "../admin/adminComponents/AdminNavbar";
+import Box from "@mui/material/Box";
+import "../styles/Dashboard.css";
+import List from "./adminBin/List";
+
+function AdminBin() {
+
+  const [user, setUser] = useState({ firstname: '', email: '', id: '' });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/auth/authenticated', { withCredentials: true })
+      .then(res => {
+        if (res.data.authenticated) {
+          setUser(res.data.user);
+        } else {
+          navigate('/login');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [navigate]);
+
+
+  return (
+    <div className="backgroundColor">
+      <Navbar />
+      <Box height={70} />
+      <Box sx={{ display: "flex" }}>
+        <Sidenav />
+
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <List />
+        </Box>
+      </Box>
+    </div>
+  );
+}
+
+export default AdminBin;
