b43b194e39f37283c4ea81cdcc5bbf908330f7ab
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-05-15 00:18:56 +0530

    driver collection created

---
+++ src/driver/driverCollection/CollectionList.jsx
@@ -0,0 +1,259 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { Divider, Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Modal from "@mui/material/Modal";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Swal from "sweetalert2";
+import EditCollection from "./EditCollection";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function CollectionList() {
+  const [user, setUser] = useState();
+  const [userId, setUserId] = useState();
+  const navigate = useNavigate();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rows, setRows] = useState([]);
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const [editCollectionId, setEditCollectionId] = useState(null);
+
+  const handleOpenEditModal = (collectionId) => {
+    setEditCollectionId(collectionId);
+    setOpenEditModal(true);
+  };
+
+  const handleCloseEditModal = () => setOpenEditModal(false);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/auth/authenticated", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (res.data.authenticated) {
+          setUser(res.data.user);
+          driverId(res.data.user.id);
+
+        } else {
+          navigate("/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const driverId = async (userId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/driver/${userId}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch driver id");
+      }
+      const data = await response.json();
+      setUserId(data.driver_id);
+      fetchCollections(data.driver_id); // Assuming data is an array of objects
+    } catch (error) {
+      console.error("Error fetching driver id:", error);
+    }
+  };
+  const fetchCollections = async (userId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/driver/collection/${userId}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch collections");
+      }
+      const data = await response.json();
+      setRows(data); // Assuming data is an array of objects
+    } catch (error) {
+      console.error("Error fetching collections:", error);
+    }
+  };
+  
+  const filterData = (v) => {
+    if (v) {
+      setRows([v]);
+    } else {
+      setRows([]);
+      fetchCollections();
+      window.location.reload();
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Modal
+          open={openEditModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <EditCollection
+              closeEvent={handleCloseEditModal}
+              collectionId={editCollectionId}
+            />
+          </Box>
+        </Modal>
+      </div>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{ padding: "20px" }}
+        >
+          Bin Collection History
+        </Typography>
+        <Divider />
+        <Box height={10} />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "20px",
+            marginRight: "20px",
+          }}
+        >
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={rows}
+            sx={{ width: 300, paddingLeft: "20px" }}
+            onChange={(e, v) => {
+              filterData(v);
+            }}
+            getOptionLabel={(rows) => rows.collection_name || ""}
+            renderInput={(params) => (
+              <TextField {...params} label="Search by name" />
+            )}
+          />
+        </div>
+
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Collection Name
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Bin Name
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Quantity (Kg)
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Material Name
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Create Date
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Update Date
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Action
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={row.code}
+                  >
+                    <TableCell key={row.id} align={"left"}>
+                      {row.collection_name}
+                    </TableCell>
+                    <TableCell key={row.id} align={"left"}>
+                      {row.bin_name}
+                    </TableCell>
+                    <TableCell key={row.id} align={"left"}>
+                      {row.quantity}
+                    </TableCell>
+                    <TableCell key={row.id} align={"left"}>
+                      {row.material_name}
+                    </TableCell>
+                    <TableCell key={row.id} align={"left"}>
+                      {row.created_at}
+                    </TableCell>
+                    <TableCell key={row.id} align={"left"}>
+                      {row.updated_at}
+                    </TableCell>
+                    <TableCell align={"center"}>
+                      <Stack spacing={2}>
+                        <EditIcon
+                          style={{
+                            fontSize: "20px",
+                            color: "#02294F",
+                            cursor: "pointer",
+                          }}
+                          className="cursor-pointer"
+                          onClick={() => handleOpenEditModal(row.collection_id)} // Pass the collection ID to the edit modal
+                        />
+                      </Stack>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </>
+  );
+}
+
---
+++ src/driver/driverCollection/EditCollection.jsx
@@ -0,0 +1,184 @@
+import { Grid, IconButton, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/Close";
+import Swal from "sweetalert2";
+import Autocomplete from "@mui/material/Autocomplete";
+
+function EditCollection({ closeEvent, orderId }) {
+  const [name, setName] = useState("");
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+  const [error, setError] = useState("");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [orderId]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/api/products");
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value);
+    calculatePrice(event.target.value, selectedProduct);
+  };
+
+  const calculatePrice = (quantity, selectedProduct) => {
+    if (selectedProduct) {
+      const totalPrice = selectedProduct.unit_price * parseInt(quantity);
+      setPrice(totalPrice);
+    }
+  };
+
+  const handleSubmit = () => {
+    // Convert quantity to a number
+    const quantityValue = parseInt(quantity);
+
+    if (isNaN(quantityValue)) {
+      setError("Quantity must be a number.");
+      return;
+    }
+
+    if (!selectedProduct) {
+      setError("Please select a product.");
+      return;
+    }
+
+    // Make PUT request to backend
+    fetch(`http://localhost:3001/api/orders/${orderId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        product_id: selectedProduct.product_id,
+        quantity: quantityValue,
+        price,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to update Order");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Order updated:", data);
+        Swal.fire("Updated!", "Your Order has been updated.", "success");
+        closeEvent();
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error updating Order:", error);
+        Swal.fire("Error!", "Failed to update the Order.", "error");
+        closeEvent();
+      });
+  };
+
+  return (
+    <div>
+      <Box sx={{ m: 2 }}></Box>
+      <Typography variant="h5" align="center">
+        Edit Order
+      </Typography>
+      <IconButton
+        style={{ position: "absolute", top: 0, right: 0 }}
+        onClick={closeEvent}
+      >
+        <CloseIcon />
+      </IconButton>
+      <Box height={20}></Box>
+      <Box height={20}></Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-basic"
+            label="Order Name"
+            variant="outlined"
+            size="small"
+            value={name}
+            onChange={handleNameChange}
+            sx={{ width: "100%" }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Autocomplete
+            value={selectedProduct}
+            onChange={(event, newValue) => {
+              setSelectedProduct(newValue);
+              calculatePrice(quantity, newValue);
+            }}
+            id="product-autocomplete"
+            options={products}
+            getOptionLabel={(option) => option.name}
+            renderInput={(params) => <TextField {...params} label="Product" />}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="outlined-basic"
+            label="Quantity"
+            variant="outlined"
+            size="small"
+            type="number"
+            value={quantity}
+            onChange={handleQuantityChange}
+            InputProps={{
+              endAdornment: "Kg",
+            }}
+            sx={{ width: "100%" }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="outlined-basic"
+            label="Price"
+            variant="outlined"
+            size="small"
+            type="text"
+            value={price}
+            readOnly
+            sx={{ width: "100%" }}
+          />
+        </Grid>
+        {error && (
+          <Grid item xs={12}>
+            <Typography variant="body2" color="error" align="center">
+              {error}
+            </Typography>
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <Typography variant="h5" align="center">
+            <Button variant="contained" onClick={handleSubmit}>
+              Update
+            </Button>
+          </Typography>
+        </Grid>
+      </Grid>
+      <Box sx={{ m: 2 }}></Box>
+    </div>
+  );
+}
+
+export default EditCollection;
