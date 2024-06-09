3f1427b759602a653b4e90c7f97b556fecd35c99
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-06-10 01:04:02 +0530

    bailer salary view

---
+++ src/bailer/BailerSalaryDetails.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Sidenav from "../bailer/bailerComponents/BailerSidenav";
+import Navbar from "../bailer/bailerComponents/BailerNavbar";
+import Box from "@mui/material/Box";
+import SalaryList from "./bailerSalary/SalaryList";
+
+function BailerSalaryDetails() {
+  return (
+    <div className="backgroundColor">
+      <Navbar />
+      <Box height={30} />
+      <Box sx={{ display: "flex" }}>
+        <Sidenav />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Box height={30} />
+          <SalaryList />
+        </Box>
+      </Box>
+    </div>
+  );
+}
+
+export default BailerSalaryDetails;
